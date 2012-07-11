f8e4155c56a4202aaec4e6115a3eaf27fd1f8ef5
pangkalizer <allan.andal@gmail.com>
2012-07-11 12:49:03 +0800

    Forced back link on creations of has one relationship

--- lib/pg_search/multisearchable.rb
+++ lib/pg_search/multisearchable.rb
@@ -11,15 +11,18 @@ module PgSearch
         :class_name => "PgSearch::Document",
         :dependent => :delete
 
-      after_create :create_pg_search_document,
-        :if => lambda { PgSearch.multisearch_enabled? }
+      after_create do
+        if PgSearch.multisearch_enabled?
+          create_pg_search_document {searchable: self}
+        end
+      end
 
       after_update :update_pg_search_document,
         :if => lambda { PgSearch.multisearch_enabled? }
     end
 
     def update_pg_search_document
-      create_pg_search_document unless self.pg_search_document
+      create_pg_search_document({searchable: self}) unless self.pg_search_document
       self.pg_search_document.save
     end
   end
